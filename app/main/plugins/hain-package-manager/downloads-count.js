63403c65b36b1ac5a12d71b04907826bfca799b3
Luke Plaster <me@lukep.org>
2016-08-15 15:08:37 +0800

    Show total download counts in /hpm install
    It was previously showing last month's count which, according to the description, is wrong.

--- app/main/plugins/hain-package-manager/downloads-count.js
+++ app/main/plugins/hain-package-manager/downloads-count.js
@@ -3,11 +3,27 @@
 const lo_assign = require('lodash.assign');
 const got = require('got');
 
-function _getDownloadsCount(packages, period) {
+const RANGE_ALL = '1000-01-01:3000-01-01';
+const PERIOD_LAST_MONTH = 'last-month';
+
+function _getDownloadCountForPoint(packages, period) {
   const query = packages.join(',');
   const query_enc = encodeURIComponent(query);
   const baseUrl = `http://api.npmjs.org/downloads/point/${period}/${query_enc},`
   return got(baseUrl, { json: true }).then(res => res.body);
+}
+
+function _getDownloadCountForRange(packages, range) {
+  const query = packages.join(',');
+  const query_enc = encodeURIComponent(query);
+  const baseUrl = `http://api.npmjs.org/downloads/range/${range}/${query_enc},`
+  return got(baseUrl, { json: true }).then(res => {
+    const { body } = res;
+    Object.keys(body).forEach(pkg => {
+      body[pkg].downloads = body[pkg].downloads.reduce((sum, dl) => sum + dl.downloads, 0);
+    });
+    return body;
+  });
 }
 
 function splitIntoChunks(arr, chunkSize) {
@@ -24,7 +40,12 @@ function getDownloadsCount(packages, period) {
   const packageChunks = splitIntoChunks(packages, 20);
   const promises = [];
   for (const packageChunk of packageChunks) {
-    const promise = _getDownloadsCount(packageChunk, period || 'last-month');
+    let promise;
+    if (! period) {
+      promise = _getDownloadCountForRange(packageChunk, RANGE_ALL);
+    } else {
+      promise = _getDownloadCountForPoint(packageChunk, period || PERIOD_LAST_MONTH);
+    }
     promises.push(promise);
   }
   return Promise.all(promises).then(results => {
